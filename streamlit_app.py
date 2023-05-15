b8c9d35cd3bca35a29a007fd916cd6a0e0216918
jubargra <65262373+jubargra@users.noreply.github.com>
2023-05-15 09:07:22 -0600

    Update streamlit_app.py
    Added feature so users can pick fruits from the list and order smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# set index to 'Fruit' so users can pick a fruit instead of id when using the list in line 17
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.title('Breakfast Favorites')
 
 streamlit.header('Breakfast Menu')
@@ -9,6 +13,9 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(my_fruit_list)
