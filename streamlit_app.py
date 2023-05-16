49c37512caef3c709971da9c608803a408ce1bc3
jubargra <65262373+jubargra@users.noreply.github.com>
2023-05-16 16:10:19 -0600

    Update streamlit_app.py
    Introduced text input for the user to call API

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,13 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
+#Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+fruit_choice= streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # takes the json, normalizes it, stores in new variable 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
