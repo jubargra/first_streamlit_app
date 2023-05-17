5764b7c1e6f2036e824de9289eb682f79de1a158
jubargra <65262373+jubargra@users.noreply.github.com>
2023-05-16 18:10:40 -0600

    Update streamlit_app.py
    reorganized imports to top of document
    
    introduced streamlit.stop()

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # set index to 'Fruit' so users can pick a fruit instead of id when using the list in line 17
@@ -29,7 +33,6 @@ fruit_choice= streamlit.text_input('What fruit would you like information about?
 streamlit.write('The user entered', fruit_choice)
 
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # takes the json, normalizes it, stores in new variable 
@@ -37,7 +40,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # uses datraframe(variable) to display it in a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+# don't run anything past this
+streamlit.stop()
+
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
